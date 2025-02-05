0780595e8dd45f6326e3f034bd3517524b323544
NiverMtz <nivermartinezmed@gmail.com>
2025-02-05 01:19:12 -0600

    feat-home-responsive

--- src/components/BannerComponent.vue
+++ src/components/BannerComponent.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="banner">
     <div class="content">
-      <h2 class="m-0 text-xl xl:text-3xl font-light">
+      <h2 class="m-0 mt-6 text-sm md:text-xl xl:text-3xl font-light">
         Te damos la bienvenida a
       </h2>
-      <h1 class="m-0 text-8xl">UTEL GAMING</h1>
+      <h1 class="m-0 text-4xl md:text-8xl">UTEL GAMING</h1>
       <Button label="Comenzar" class="mt-8"></Button>
     </div>
   </div>
@@ -17,16 +17,22 @@
     url("../assets/images/banner-bg.jpg");
   background-size: cover;
   display: flex;
-  justify-content: start;
+  justify-content: center;
+  @media screen and (min-width: 960px) {
+    justify-content: start;
+  }
   color: white;
 }
 
 .content {
-  padding: 4rem;
-  padding-left: 10rem;
-  max-width: 40vw;
+  max-width: 100%;
   display: flex;
   flex-direction: column;
+  @media screen and (min-width: 960px) {
+    padding: 4rem;
+    padding-left: 10rem;
+    max-width: 40vw;
+  }
 }
 
 .title {
