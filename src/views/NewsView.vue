d6d34325f0f657df0f2cb099e1e1b50afbf7731f
NiverMtz <nivermartinezmed@gmail.com>
2025-02-05 19:04:26 -0600

    fix responsive news images

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card p-8">
+  <div class="card w-full pt-8 lg:p-8">
     <h2 class="m-0 m-4 text-6xl">Noticias</h2>
     <DataView :value="news">
       <template #list="slotProps">
@@ -17,16 +17,16 @@
             >
               <div class="col-12">
                 <div class="flex justify-content-between">
-                  <h3 class="my-3 text-6xl">{{ item.title }}</h3>
+                  <h3 class="my-3 lg:text-6xl">{{ item.title }}</h3>
                   <small class="align-self-end mb-4" style="color: grey">{{
                     item.type
                   }}</small>
                 </div>
               </div>
-              <div class="col-4">
+              <div class="col-12 md:col-4">
                 <Image :src="item.url" alt="Image" width="100%" preview />
               </div>
-              <div class="col-8">
+              <div class="col-12 md:col-8">
                 <div class="flex justify-content-between">
                   <p class="my-0">
                     {{ item.description }}
